f23c1555ee215886d1fef56761dac5e81de24f9b
Carter Capocaccia <carter.capocaccia@gmail.com>
2022-08-03 19:04:50 -0500

    moves base url to config file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.get('h1').should('contain.text', 'Next').should('be.visible')
   })
 })
